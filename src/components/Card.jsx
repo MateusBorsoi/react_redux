57755aa3bddcc751c5b2b30850fa8df7ee89ae81
Mateus Borsoi <borsoimateus@gmail.com>
2023-05-15 16:04:33 -0300

    update

---
+++ src/components/Card.jsx
@@ -0,0 +1,25 @@
+import "./Card.css";
+
+const Card = (props) => {
+  const get_colors = (props) => {
+
+
+    if (props.Red) return 'Red'
+    if (props.Green) return 'Green'
+    if (props.Blue) return 'Blue'
+    if (props.Purple) return 'Purple'
+    return ""
+  };
+
+  return (
+    <div className={`Card ${get_colors(props)}`}>
+      <div className="Header">
+        <span className="Title">{props.title}</span>
+      </div>
+      <div className="Content"></div>
+      {props.children}
+    </div>
+  );
+};
+
+export default Card;

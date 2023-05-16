65140007f0eab9e3ad8519e41891daa9529ae2a6
Mateus Borsoi <borsoimateus@gmail.com>
2023-05-16 10:53:27 -0300

    todo

--- src/store/actions/numeros.js
+++ src/store/actions/numeros.js
@@ -1,13 +1,15 @@
+import { NUM_MIN_ALT, NUM_MAX_ALT } from "../actions/actionTypes";
+
 export function alterarNumeroMinimo(novoNumero) {
   return {
-    type: "NUM_MIN_ALT",
+    type: NUM_MIN_ALT,
     payload: novoNumero,
   };
 }
 
 export function alterarNumeroMaximo(novoNumero) {
   return {
-    type: "NUM_MAX_ALT",
+    type: NUM_MAX_ALT,
     payload: novoNumero,
   };
 }
--- src/store/reducers/numeros.js
+++ src/store/reducers/numeros.js
@@ -1,3 +1,5 @@
+import { NUM_MIN_ALT, NUM_MAX_ALT } from "../actions/actionTypes";
+
 const InitialState = {
   min: 0,
   max: 0,
@@ -5,12 +7,12 @@ const InitialState = {
 
 export default function (state = InitialState, action) {
   switch (action.type) {
-    case "NUM_MIN_ALT":
+    case NUM_MIN_ALT:
       return {
         ...state,
         min: action.payload,
       };
-    case "NUM_MAX_ALT":
+    case NUM_MAX_ALT:
       return {
         ...state,
         max: action.payload,
